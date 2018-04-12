914ba2d4b494a3eaccb9d6099d0f77bb1afa23bf
Clifford Crerar <cliff.crerar@gmail.com>
2018-04-12 20:38:00 +0200

    Working on tasks

--- js/more.js
+++ js/more.js
@@ -5,7 +5,7 @@
  * @author Cliff Crerar
  *
  * Created at     : 2018-04-10 22:34:47 
- * Last modified  : 2018-04-11 17:59:59
+ * Last modified  : 2018-04-12 20:03:58
  */
 
  $(window).load(function(){
@@ -13,3 +13,9 @@
         $('#loadingDiv').fadeOut(2000);
      }, 2000);
  });
+
+ /* Functions that must run when document is ready */
+
+$(document).ready(function(){
+    $('.nav-menu').onePageNav();
+});

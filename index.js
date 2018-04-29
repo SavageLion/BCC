707a366ae2d876d70081cff4ef232c12cdbfbb9c
Clifford Crerar <cliff.crerar@gmail.com>
2018-04-29 18:03:05 +0200

    installing packages:

---
+++ index.js
@@ -0,0 +1,44 @@
+/**
+ * long description for the file
+ *
+ * @summary Main entry point for BCC
+ * @author Cliff Crerar
+ *
+ * Created at     : 2018-04-29 17:32:45 
+ * Last modified  : 2018-04-29 17:56:48
+ */
+
+/****************************************/
+/*      LIBRARIES AND FRAMEWORKS        */
+/****************************************/
+
+//<!--JQUERY-->
+//<script src="node_modules/jquery/dist/jquery.js"></script>
+import $ from 'script-loader!./node_modules/jquery/dist/jquery.js';
+import jQuery from 'script-loader!./node_modules/jquery/dist/jquery.js';
+
+/****************************************/
+/*          TEMPLATE CSS FILES          */
+/****************************************/
+
+import './css/_fonts.css'; // font css file
+//<!-- Bootstrap CSS File -->
+//<link href="./node_modules/bootstrap/dist/css/bootstrap.css" rel="stylesheet">
+//<!--<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
+
+//<!-- Libraries CSS Files -->
+//<link href="lib/animate/animate.min.css" rel="stylesheet">
+//<link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+//<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+//<link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+//<!-- Main Stylesheet File -->
+//<link href="css/style.css" rel="stylesheet">
+
+//<!--Additional CSS by IA-->
+//<link rel="stylesheet" type="text/css" href="./css/_more.css">
+//<link rel="stylesheet" type="text/css" href="./css/_alerts.css">
+
+/****************************************/
+/*          TEMPLATE JS FILES           */
+/****************************************/
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,1 +1,25 @@
 const path = require('path');
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            { test: /\.(png|jpg|gif)$/, use: [{ loader: 'url-loader', options: { limit: 8192 } }] },
+            { test: /\.(png|jpg|gif)$/, use: [{ loader: 'file-loader', options: {} }] },
+            { test: /\.(html)$/, use: { loader: 'html-loader', options: { attrs: [':data-src'] } } },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+        ],
+    },
+
+    plugins: [
+        new HTMLWebPackPlugin({
+            template: './index.html',
+            favicon: './img/bcc_vector_xQ3_icon.ico'
+        })
+    ]
+};
